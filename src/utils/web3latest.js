f76e09cf4773db82e6fb1a60364609c4fa5a5fc7
RobertoC27 <chi13027@uvg.edu.gt>
2018-09-19 17:49:54 -0600

    updated rpc node provider

--- src/utils/web3latest.js
+++ src/utils/web3latest.js
@@ -6,7 +6,7 @@ if (typeof window.web3 !== 'undefined') {
     console.log('Metamask');
 } else {
     // If no injected web3 instance is detected, fallback to AWS instance (nodo-1).    
-    var provider = new Web3.providers.HttpProvider('http://54.71.80.178:8989');
+    var provider = new Web3.providers.HttpProvider('http://35.165.129.25:8989');
     console.log('AWS');
     web3latest = new Web3(provider)
 }
